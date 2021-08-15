b33e78babf1e473a52f30ec01214405f56de38b2
Anmol Gupta <anmolgupta520@gmail.com>
2021-08-15 23:11:55 +0530

    added c_language

---
+++ c_language/sample.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// sample comment
+
+int main()
+{
+	// sample comment 2
+	int a = 50;
+	int b = 60;
+	int c = a + b;
+	// sample comment 3
+	printf("%i",c);
+	return 1;
+
+	// sample comment
+}
+// sample comment
+// sample comment
+// sample comment// sample comment
