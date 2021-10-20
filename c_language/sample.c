2c8e451b29238a4ed76c01a889bbf5beb59eb5e4
Anmol Gupta <anmolgupta520@gmail.com>
2021-10-20 15:29:15 +0530

    added assignments

--- c_language/sample.c
+++ c_language/sample.c
@@ -8,8 +8,10 @@ int main()
 	int a = 50;
 	int b = 60;
 	int c = a + b;
+	float d = 1.16180;
 	// sample comment 3
 	printf("%i",c);
+	printf("Golden Ratio : %f",d);
 	return 1;
 
 	// sample comment
